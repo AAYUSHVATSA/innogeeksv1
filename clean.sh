bc53d78eeb43f36f5cf2b58e0e0daf73d363cabd
AAYUSHVATSA <aayushvatsa02@gmail.com>
2022-02-19 21:10:24 +0530

    first funtion done

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "first functionality initiated"
+echo "cleaning started..."
+sudo apt-get autoclean
+sudo apt-get clean 
+sudo apt-get autoremove
+echo "cleaning completed"
+echo "thanks for using this functionality"
